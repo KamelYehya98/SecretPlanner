fd2da16195a4b7e0ed89c1983078109cd52ea264
Nirvana <kamel.yehya04@gmail.com>
2023-06-14 09:12:28 +0300

    update comments model

--- models/Comments.js
+++ models/Comments.js
@@ -7,7 +7,7 @@ const commentsSchema = new mongoose.Schema({
     },
     createdAt:{
         type: Date,
-        default: Date.now()
+        default: () => Date.now()
     },
     user: {
         type: Object,
